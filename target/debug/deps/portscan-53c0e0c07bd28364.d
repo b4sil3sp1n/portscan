3e6b0519e5e9ec28bdcad59b3a40a02bbc2a3cc1
b4sil3sp1n <basile.espin.zab@gmail.com>
2026-01-12 22:38:22 +0100

    Delete target directory

--- target/debug/deps/portscan-53c0e0c07bd28364.d
+++
@@ -1,5 +0,0 @@
-/home/basile/Documents/rust/portscan/target/debug/deps/portscan-53c0e0c07bd28364.d: src/main.rs
-
-/home/basile/Documents/rust/portscan/target/debug/deps/portscan-53c0e0c07bd28364: src/main.rs
-
-src/main.rs:
--- target/debug/portscan.d
+++
@@ -1,1 +0,0 @@
-/home/basile/Documents/rust/portscan/target/debug/portscan: /home/basile/Documents/rust/portscan/src/main.rs
